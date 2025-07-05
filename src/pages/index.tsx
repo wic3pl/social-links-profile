d8f6e03a133a9737070f744d604d3ef03d046873
wic3p <wiktoria.pilecka@wp.pl>
2025-07-05 15:17:28 +0200

    Social Link Profile quick fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className="justify-items-center h-screen grid items-center">
-      <div className="bg-neutral-900 rounded-2xl items-center px-12 justify-items-center w-96 h-2/3 ">
+    <div className="justify-items-center px-4 h-screen grid items-center">
+      <div className="bg-neutral-900 rounded-2xl flex flex-col items-center px-12 py-10 w-96 min-h-96 ">
         <Image
           className="rounded-full mt-10"
           src="/avatar-jessica.jpeg"
@@ -18,7 +18,7 @@ export default function Home() {
         <div className="mt-6 text-sm font-extralight">
           "Front-end developer and avid reader"
         </div>
-        <div className="grid grid-rows-5 gap-2 font-sans text-sm font-bold  h-1/2 mt-6">
+        <div className="grid grid-rows-5 gap-4 w-full font-sans text-sm font-bold   mt-6">
           <button className="bg-neutral-800 hover:text-black hover:bg-lime-400 h-12  rounded-lg w-72">
             GitHub
           </button>
